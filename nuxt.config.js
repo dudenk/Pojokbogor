ec8960939a54b4d5c8a326c561228843fdce212f
Gun Gun Gumilar <gg.gumilar@gmail.com>
2018-03-17 07:16:51 +0700

    correct manifest icons src

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,31 +72,31 @@ module.exports = {
     description: 'Berita dan kabar pilihan terkini serta terbaru seputar peristiwa, kuliner, belanja, dan wisata di Bogor.',
     lang: 'id',
     icons: [{
-      "src": "@static/icons/icon-72x72.png",
+      "src": "/icons/icon-72x72.png",
       "sizes": "72x72",
       "type": "image/png"
     }, {
-      "src": "@static/icons/icon-96x96.png",
+      "src": "/icons/icon-96x96.png",
       "sizes": "96x96",
       "type": "image/png"
     }, {
-      "src": "@static/icons/icon-144x144.png",
+      "src": "/icons/icon-144x144.png",
       "sizes": "144x144",
       "type": "image/png"
     }, {
-      "src": "@static/icons/icon-168x168.png",
+      "src": "/icons/icon-168x168.png",
       "sizes": "168x168",
       "type": "image/png"
     }, {
-      "src": "@static/icons/icon-192x192.png",
+      "src": "/icons/icon-192x192.png",
       "sizes": "192x192",
       "type": "image/png"
     }, {
-      "src": "@static/icons/icon-256x256.png",
+      "src": "/icons/icon-256x256.png",
       "sizes": "256x256",
       "type": "image/png"
     }, {
-      "src": "@static/icons/icon-512x512.png",
+      "src": "/icons/icon-512x512.png",
       "sizes": "512x512",
       "type": "image/png"
     }],
